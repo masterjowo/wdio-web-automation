ec2aab1cad1a601ecc62ec5bd308e74e4480a2fe
masterjowo <daniajaOK@gmail.com>
2025-05-06 13:50:54 +0700

    updatev1 readme.md

--- pageobjects/Page.js
+++ pageobjects/Page.js
@@ -9,7 +9,7 @@ export default class Page {
      * Opens a sub page of the page
      * @param {string} path - path of the sub page (e.g. /path/to/page.html)
      */
-
+    //
     open(path) {
 
         return browser.url(path)
