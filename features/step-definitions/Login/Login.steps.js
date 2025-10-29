aabd48010948b977c27d072993e95c6b6866907f
Ramdhani <daniajaOK@gmail.com>
2025-10-29 22:04:17 +0700

    udapte step definitions v11

--- features/step-definitions/Login/Login.steps.js
+++ features/step-definitions/Login/Login.steps.js
@@ -15,52 +15,52 @@ import fs from 'fs';
 //     await allureReporter.addStory('saya melakukan login ke halaman vini');
 // });
 
-// When('saya login melakukan {string} dan {string}', async (username, password) => {
+When('saya login melakukan {string} dan {string}', async (username, password) => {
 
-//     await loginPage.login(username, password);
-//     const events = [];
+    await loginPage.login(username, password);
+    const events = [];
 
-//     browser.on('network.responseCompleted', (params) => {
-//     events.push({
-//         time: Date.now(),
-//         url: params.request?.url || params.response?.url,
-//         status: params.response?.status,
-//         mime: params.response?.mimeType || null
-//     });
-//     });
+    browser.on('network.responseCompleted', (params) => {
+    events.push({
+        time: Date.now(),
+        url: params.request?.url || params.response?.url,
+        status: params.response?.status,
+        mime: params.response?.mimeType || null
+    });
+    });
 
-//     // di after hook atau akhir test
-//     after(async () => {
-//     fs.writeFileSync('./network-events.json', JSON.stringify(events, null, 2));
-//     });
-//     // const metricsMap = {};
-//     // metrics.forEach(metric => {
-//     //     metricsMap[metric.name] = metric.value;
-//     // });
+    // di after hook atau akhir test
+    after(async () => {
+    fs.writeFileSync('./network-events.json', JSON.stringify(events, null, 2));
+    });
+    // const metricsMap = {};
+    // metrics.forEach(metric => {
+    //     metricsMap[metric.name] = metric.value;
+    // });
 
-//     //console.log('📈 Performance Metrics:', metricsMap);
+    //console.log('📈 Performance Metrics:', metricsMap);
 
-//     // Kirim metrics ini ke Allure report sebagai attachment
-//     // await browser.call(async () => {
-//     //     const allure = await import('@wdio/allure-reporter').then(m => m.default || m);
-//     //     allure.addAttachment(
-//     //         'Performance Metrics',
-//     //         JSON.stringify(metricsMap, null, 2), // Dibuat pretty-print JSON
-//     //         'application/json'
-//     //     );
-//     // });
+    // Kirim metrics ini ke Allure report sebagai attachment
+    // await browser.call(async () => {
+    //     const allure = await import('@wdio/allure-reporter').then(m => m.default || m);
+    //     allure.addAttachment(
+    //         'Performance Metrics',
+    //         JSON.stringify(metricsMap, null, 2), // Dibuat pretty-print JSON
+    //         'application/json'
+    //     );
+    // });
 
-//     // // Kalau mau, bisa juga tambahin beberapa step manual ke Allure
-//     // await browser.call(async () => {
-//     //     const allure = await import('@wdio/allure-reporter').then(m => m.default || m);
-//     //     allure.addStep('First Meaningful Paint: ' + metricsMap.FirstMeaningfulPaint + ' ms');
-//     //     allure.addStep('DomContentLoaded: ' + metricsMap.DomContentLoaded + ' ms');
-//     // });
+    // // Kalau mau, bisa juga tambahin beberapa step manual ke Allure
+    // await browser.call(async () => {
+    //     const allure = await import('@wdio/allure-reporter').then(m => m.default || m);
+    //     allure.addStep('First Meaningful Paint: ' + metricsMap.FirstMeaningfulPaint + ' ms');
+    //     allure.addStep('DomContentLoaded: ' + metricsMap.DomContentLoaded + ' ms');
+    // });
 
 
-//     // await allureReporter.addFeature(' Halaman login');
-//     // await allureReporter.addStory(`saya login melakukan ${username} dan ${password}`);
-// });
+    // await allureReporter.addFeature(' Halaman login');
+    // await allureReporter.addStory(`saya login melakukan ${username} dan ${password}`);
+});
 Given('user sudah berada pada halaman awal, user dapat melihat {string}', async (string) => {
     const data = await loc.text.waitForDisplayed({ timeout: 5000 });
     const element = loc.text; // WebdriverIO element
@@ -81,7 +81,7 @@ Then('user dapat melihat pesan {string}', async (message) => {
     await allureReporter.addFeature(' Halaman login');
     await allureReporter.addStory(`user dapat melihat pesan ${message}`);
 });
-When(/^saya login melakukan pengisian (.*) dan (.*)$/,async (username, password) => {
-  // Write code here that turns the phrase above into concrete actions
-  await loginPage.login(username, password);
-})
+// When(/^saya login melakukan pengisian (.*) dan (.*)$/,async (username, password) => {
+//   // Write code here that turns the phrase above into concrete actions
+//   await loginPage.login(username, password);
+// })
