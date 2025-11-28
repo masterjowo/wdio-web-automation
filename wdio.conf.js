3a3c3db5c6fe0288994447107249f13bfee3385b
masterjowo <daniajaok@gmail.com>
2025-11-28 15:48:45 +0700

    28/11/25

--- wdio.conf.js
+++ wdio.conf.js
@@ -152,6 +152,7 @@ export const config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
+    //
     reporters: ['spec', ['allure', {
         outputDir: 'allure-results',
         disableWebdriverStepsReporting: false,
